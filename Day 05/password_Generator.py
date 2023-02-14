0cb046d8f643d3cb25589b40d40db9d3f881b30c
Weerasinghe C.C <chehanwe1998@gmail.com>
2023-02-14 23:12:43 +0530

    updated password_generator final update

--- Day 05/password_Generator.py
+++ Day 05/password_Generator.py
@@ -14,6 +14,7 @@ nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 print("")
+
 # Eazy Level - Order not randomised:
 # e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 
